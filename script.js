d71c228677c41bd930dc5f4e66feda9492b44333
matheusmedeiros4 <125043947+matheusmedeiros4@users.noreply.github.com>
2023-08-06 17:54:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,44 +2,82 @@ const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
 const clouds = document.querySelector('.clouds')
 const gameOver = document.querySelector('.game-over')
-const start = document.querySelector('.start')
+const startText = document.querySelector('.start')
+
+let gameStarted = false
+
+const start = () => {
+    mario.style.bottom = `0px`
+    mario.src = 'images/mario.gif'
+    mario.style.width = '150px'
+    
+    pipe.style.rigth = `-80px`
+    mario.style.left = 'initial'
+    pipe.style.left = `initial`
+    clouds.style.left = `initial`
 
 
-const loop = setInterval(() => {
+
+    startText.style.display = 'none'
+    gameOver.style.display = 'none'
+
+    document.removeEventListener('keydown', start)
+
+    pipe.classList.add('pipe-move')
+    clouds.classList.add('clouds-move')
+
+    const loop = setInterval(() => {
         const pipePosition = pipe.offsetLeft
         const cloudsPosition = clouds.offsetLeft
 
+        
+
         const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
-        if(pipePosition <= 125 && pipePosition > 0 && marioPosition < 60) {
-            clouds.style.animation = 'none'
+        if (pipePosition <= 125 && pipePosition > 0 && marioPosition < 60) {
             clouds.style.left = `${cloudsPosition}px`
 
-            pipe.style.animation = 'none'
             pipe.style.left = `${pipePosition}px`
 
-            mario.style.animation = 'none'
+            // mario.style.animation = 'none'
             mario.style.bottom = `${marioPosition}px`
 
-            mario.src = 'game-over.png'
+            pipe.classList.remove('pipe-move')
+            clouds.classList.remove('clouds-move')
+
+            mario.src = 'images/game-over.png'
             mario.style.width = '75px'
             mario.style.left = '50px'
 
             gameOver.style.display = 'block'
 
+            gameStarted = false
+            startText.style.display = 'block'
+
             clearInterval(loop)
         }
 
-}, 10)
+    }, 10)
 
-const jump = () => {
-    mario.classList.add('jump')
+    let jumpInProgress = false;
+    const jump = () => {
+        if (!jumpInProgress) {
+            jumpInProgress = true;
+            mario.classList.add('jump');
     
-    setTimeout(() => {
-        mario.classList.remove('jump')
-    }, 500) 
+            setTimeout(() => {
+                mario.classList.remove('jump');
+                jumpInProgress = false;
+            }, 500);
+        }
+    }
 
+    document.addEventListener('keydown', jump)
 }
 
-
-document.addEventListener('keydown', jump)
+document.addEventListener('keydown', () => {
+    if (!gameStarted) {
+        gameStarted = true;
+        start()
+    }
+})
